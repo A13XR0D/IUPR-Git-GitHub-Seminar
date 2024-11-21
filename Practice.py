9a7068910a6fea46681e18e2ce208ed5e43dcc28
A13XR0D <141086123+A13XR0D@users.noreply.github.com>
2024-11-21 01:05:11 -0400

    Added example code for the PowerPoint

--- Practice.py
+++ Practice.py
@@ -1,4 +1,5 @@
 class Practice:
    #Please write your code below the example.
    print("Juan del Pueblo, 26, Computer Science, 3rd") #Example
+   print("Jose del Valle, 27, Computer Engineering, 2nd")#Example for Pp
     
