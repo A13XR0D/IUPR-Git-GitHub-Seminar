f2e7ac7dc50bb8324f4abbb0129121ff3e42acc7
LuxJustin01 <luxjustin01@gmail.com>
2024-11-22 10:53:42 -0400

    Justin contribution

--- Practice.py
+++ Practice.py
@@ -1,4 +1,4 @@
 class Practice:
    #Please write your code below the example.
    print("Juan del Pueblo, 26, Computer Science, 3rd") #Example
-    
+   print("Justin Ferrer, 21, Computer Science 4th")
